a5341ad925029cf400b9bfd3832b1ea1a8616755
Jojnas <jan.stancek49@gmail.com>
2016-10-18 09:22:17 +0200

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,28 @@
 /**
  * Created by jano on 10/17/2016.
  */
+$(function () {
+   // cache window object
+
+    var $window = $(window);
+    $('section[data-type="background"]').each(function () {
+        var $bgobj = $(this); // assigning object
+
+        $(window).scroll(function () {
+            // scroll the background at var speed
+            // the yPos is a negative value because we are scrolling it UP
+
+            var yPos = -($window.scrollTop) / $bgobj.data('speed');
+
+            // put together our final background position
+
+            var coords = '50%' + yPos + 'px';
+
+            // move the background
+
+            $bgobj.css({backgroundPosition: coords});
+
+
+        })
+    })
+});
